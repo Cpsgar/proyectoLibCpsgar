f0eac98207b7e9eba688e59c3748405813754cd4
Cpsgar <cesardim45@gmail.com>
2023-03-14 13:26:12 +0100

    first commit libreria

---
+++ src/main/java/es/mde/acing/ProyectoLibCpsgarApplication.java
@@ -0,0 +1,8 @@
+package es.mde.acing;
+
+public class ProyectoLibCpsgarApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/main/java/es/mde/acing/utils/pruebaLibreria.java
@@ -0,0 +1,8 @@
+package es.mde.acing.utils;
+
+public class pruebaLibreria {
+
+	public static void imprime() {
+		System.out.println("hola libreria");
+	}
+}
